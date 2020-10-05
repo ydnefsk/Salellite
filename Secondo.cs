e6fe1fd3692cee99b1071497130b9ac355a0630f
ydnefsk <mattia.fumagalli@issgreppi.it>
2020-10-05 10:08:21 +0200

    Seconda commit

---
+++ Secondo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secondo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Writeline("Mi chiamo Mattia Fumagalli");
+        }
+    }
+}
